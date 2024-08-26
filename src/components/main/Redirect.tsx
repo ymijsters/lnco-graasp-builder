3359b3ad544253723a667d6ed786aa01c409af3d
LAPTOP-YANNICK\ymijs <y.mijsters@gmail.com>
2024-08-26 15:54:25 +0200

    fix: remove redirect

--- src/components/main/Redirect.tsx
+++ src/components/main/Redirect.tsx
@@ -1,13 +1,15 @@
 import { useEffect } from 'react';
 
-import { redirectToSavedUrl } from '@graasp/sdk';
+// import { redirectToSavedUrl } from '@graasp/sdk';
 import { RedirectionContent } from '@graasp/ui';
 
 import { HOME_PATH } from '../../config/paths';
 
 const Redirect = (): JSX.Element => {
   useEffect(() => {
-    redirectToSavedUrl(window, HOME_PATH);
+    // redirectToSavedUrl(window, HOME_PATH);
+    // eslint-disable-next-line no-console
+    console.log('test');
   }, []);
 
   return <RedirectionContent link={HOME_PATH} />;
